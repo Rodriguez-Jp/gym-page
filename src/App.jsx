21634fd014d679a17ea8d51b7ba4871c3154fad6
Juan Pablo Rodriguez Becerra <juanrdrz16@gmail.com>
2023-06-05 15:15:21 -0500

    Become a member section finished

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,22 @@ function App() {
       <main>
         <FrontPage />
         <Program />
+        <section className="bg-[url('/public/media/cta-bg.jpeg')] min-w-full bg-cover bg-no-repeat h-[379px]">
+          <div className="mx-auto text-center  text-white max-w-4xl h-full flex flex-col justify-center items-center">
+            <h1 className="text-3xl mt-10 mb-6 font-bold uppercase">
+              don't <span className="text-red-500">think</span>, begin{" "}
+              <span className="text-red-500">today</span>!
+            </h1>
+            <p className="mb-8 font-normal text-md">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, in
+              hic ullam molestias quae quos, laboriosam, atque modi molestiae
+              natus magnam impedit ipsam suscipit!
+            </p>
+            <button className="uppercase bg-red-500 hover:opacity-80 p-4 font-normal">
+              <a href="#">Become a member</a>
+            </button>
+          </div>
+        </section>
       </main>
     </>
   );
